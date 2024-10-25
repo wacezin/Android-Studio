1a94b3bedf972c0a896ccad7c94a334cf63a7866
Taina <taina.kotona02@gmail.com>
2024-10-25 14:50:29 -0300

    Terminando Projeto08

--- Projeto08/app/src/main/java/com/teines/projeto08/MainActivity.java
+++ Projeto08/app/src/main/java/com/teines/projeto08/MainActivity.java
@@ -1,6 +1,8 @@
 package com.teines.projeto08;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,60 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputEditText edit_precoAlcool, edit_precoGasolina;
+    private TextView text_resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        edit_precoAlcool = findViewById(R.id.edit_precoAlcool);
+        edit_precoGasolina = findViewById(R.id.edit_precoGasolina);
+        text_resultado = findViewById(R.id.text_resultado);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void calcularPreco(View view){
+        String precoAlcool = edit_precoAlcool.getText().toString();
+        String precoGasolina = edit_precoGasolina.getText().toString();
+
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+
+        if(camposValidados){
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+
+            Double resultado = valorAlcool / valorGasolina;
+
+            if(resultado >= 0.7){
+                text_resultado.setText("Melhor utilizar GASOLINA");
+            }else {
+                text_resultado.setText("Melhor utilizar ÁLCOOL");
+            }
+        }else{
+            text_resultado.setText("Preencha os campos!");
+        }
+    }
+
+    public Boolean validarCampos(String pAlcool, String pGasolina){
+        boolean camposValidados = true; // CAMPOS SÃO VERDADEIROS
+
+        if(pAlcool == null || pAlcool.equals("")){
+            camposValidados = false;
+        }else if(pGasolina == null || pGasolina.equals("")){
+            camposValidados = false;
+        }
+        return camposValidados;
+    }
+
 }
